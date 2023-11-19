09f51afd18d47a300c653347ed0e5c8cca390823
drkostas <georgiou.kostas94@gmail.com>
2023-11-18 20:45:03 -0500

    Restructuring

--- index.php
+++ index.php
@@ -152,9 +152,9 @@
       src="https://code.jquery.com/jquery-3.3.1.min.js"
       integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
       crossorigin="anonymous"></script>
-    <script src="../p5.min.js"></script>
-    <script src="../addons/p5.dom.min.js"></script>
-    <script src="../addons/p5.sound.min.js"></script>
+    <script src="./addons/p5.min.js"></script>
+    <script src="./addons/p5.dom.min.js"></script>
+    <script src="./addons/p5.sound.min.js"></script>
     <script src="monster.js"></script>
     <script src="player.js"></script>
     <script src="bullet.js"></script>
